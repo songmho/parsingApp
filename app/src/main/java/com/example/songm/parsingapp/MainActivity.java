4f12a615e8fbb5f95a4487c1f5c30fa84c743550
songmho <songmho@gmail.com>
2017-07-08 14:40:19 +0900

    0. json에서 필요한 데이터 가져와 json array형식으로 변형. 1. sharedPreference 로 데이터 저장 및 불러오기 추가.

--- app/src/main/java/com/example/songm/parsingapp/MainActivity.java
+++ app/src/main/java/com/example/songm/parsingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.songm.parsingapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -34,12 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         String url = "http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getMsrstnList?pageNo=1&numOfRows=400&ServiceKey=NCPIDFE%2F7buZ0eIVTd6x6iqFLtZRkGcVW%2FZuKO1g%2BM9cCN8YBQBmPIKzaP%2B9MTfyyNMhXDS3SkK8%2FjiyINYe0A%3D%3D&_returnType=json";
 
+        final SharedPreferences preferences =getSharedPreferences("Pref",MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+
+
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
                 try {
                     JSONArray jsonObject = response.getJSONArray("list");
+                    JSONArray result = new JSONArray();
+
 
                     if(jsonObject!=null) {
                         text.setText("" + jsonObject.length());
@@ -53,7 +60,22 @@ public class MainActivity extends AppCompatActivity {
                             String stationName = o.getString("stationName");
 
                             text.setText(addr+"    "+dmX+"    "+dmY+"   "+oper+"   "+stationName);
+
+                            JSONObject curObs = new JSONObject();
+                            curObs.put("addr",addr);
+                            curObs.put("dmX",dmX);
+                            curObs.put("dmY",dmY);
+                            curObs.put("oper",oper);
+                            curObs.put("stationName",stationName);
+
+                            result.put(curObs);
+
                         }
+
+                        editor.putString("Observe",result.toString());
+                        editor.commit();
+
+                        text.setText(""+preferences.getString("Observe","null"));
                     }
                     else
                         text.setText("널");
